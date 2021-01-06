518e57a8dfa0baeb96be2f64f01478dd383429ce
salim ameur <salim.ameur@gmail.com>
2021-01-06 17:18:29 +0000

    fix app.js + tour.js

--- 2-tours/setup/src/Tour.js
+++ 2-tours/setup/src/Tour.js
@@ -1,7 +1,17 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({id,name,info,image,price}) => {
+
+  return <article className="single-tour">
+   <img src={image} alt={name}/>
+  <footer>
+    <div className="tour-info">
+      <h4>{name}</h4>
+
+    </div>
+  </footer>
+
+  </article>;
 };
 
 export default Tour;
