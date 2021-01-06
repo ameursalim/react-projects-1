436d0aec21850cdbe37732a357d84408deed4d20
salim ameur <salim.ameur@gmail.com>
2021-01-06 17:36:29 +0000

    show more show less

--- 2-tours/setup/src/Tour.js
+++ 2-tours/setup/src/Tour.js
@@ -2,13 +2,23 @@ import React, { useState } from 'react';
 
 const Tour = ({id,name,info,image,price}) => {
 
+  const [displayInfo , setDisplayInfo]= useState(false)
+
+
+
+
   return <article className="single-tour">
    <img src={image} alt={name}/>
   <footer>
     <div className="tour-info">
       <h4>{name}</h4>
-
+      <h4 className="tour-price">{price}</h4>
     </div>
+    <p>{displayInfo ? info : `${info.substring(0,200)}...`}</p>
+   <button onClick={()=> setDisplayInfo(!displayInfo) }>
+     {displayInfo? 'show less':'read more'}
+   </button>
+    <button className="delete-btn"> Not interested</button>
   </footer>
 
   </article>;
